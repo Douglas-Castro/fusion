032e061d27d9d957a56d151595d383c717bd4e03
Douglas-Castro <farias.douglascastro@gmail.com>
2021-03-09 23:58:52 -0300

    :arrow_up: Update Admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Service, Position, Employee
 
-# Register your models here.
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ('position', 'active', 'modified')
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('service', 'icon', 'active', 'modified')
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'position', 'active', 'modified')
+
