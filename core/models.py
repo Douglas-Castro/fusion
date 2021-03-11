85e8f939206f2f7d2506f5291ead8a80a862c4ea
Douglas-Castro <farias.douglascastro@gmail.com>
2021-03-11 20:49:31 -0300

    :bulb: Add Feature model and update icon-choices

--- core/models.py
+++ core/models.py
@@ -3,6 +3,18 @@ from django.db import models
 from stdimage.models import StdImageField
 
 
+ICON_CHOICES = (
+    ('lni-cog', 'Engrenagem'),
+    ('lni-stats-up', 'Gráfico'),
+    ('lni-users', 'Usuário'),
+    ('lni-layers', 'Design'),
+    ('lni-mobile', 'Mobile'),
+    ('lni-rocket', 'Foguete'),
+    ('lni-laptop-phone', 'Computador&celular'),
+    ('lni-leaf', 'Folha'),
+)
+
+
 def get_file_path(_instance, filename):
     ext = filename.split('.')[-1]
     filename = f'{uuid.uuid4()}.{ext}'
@@ -20,17 +32,9 @@ class Base(models.Model):
 
 
 class Service(Base):
-    ICON_CHOICES = (
-        ('lni-cog', 'Engrenagem'),
-        ('lni-stats-up', 'Gráfico'),
-        ('lni-users', 'Usuário'),
-        ('lni-layers', 'Design'),
-        ('lni-mobile', 'Mobile'),
-        ('lni-rocket', 'Foguete'),
-    )
     service = models.CharField('Serviço', max_length=100)
     description = models.TextField('Descrição', max_length=200)
-    icon = models.CharField('Ícone', max_length=12, choices=ICON_CHOICES)
+    icon = models.CharField('Ícone', max_length=16, choices=ICON_CHOICES)
 
     class Meta:
         verbose_name = 'Serviço'
@@ -66,4 +70,18 @@ class Employee(Base):
 
     def __str__(self):
         return self.name
+
+
+class Feature(Base):
+    title = models.CharField('Título', max_length=100)
+    icon = models.CharField('Ícone', max_length=16, choices=ICON_CHOICES)
+    description = models.TextField('Descrição', max_length=300)
+
+
+    class Meta:
+        verbose_name = 'Feature'
+        verbose_name_plural = 'Features'
+
+    def __str__(self):
+        return self.title
 
