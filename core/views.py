050831c0e74f35e90071f4078730a23994bdbf6f
Douglas-Castro <farias.douglascastro@gmail.com>
2021-03-11 20:48:02 -0300

    :bulb: Add context data services, employees, features

--- core/views.py
+++ core/views.py
@@ -1,6 +1,16 @@
 from django.views.generic import TemplateView
+from .models import Service, Employee, Feature
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['services'] = Service.objects.order_by('?').all()
+        context['employees'] = Employee.objects.order_by('?').all()
+        context['featurescolumn1'] = Feature.objects.order_by('?').all()[:3]
+        context['featurescolumn2'] = Feature.objects.order_by('?').all()[3:6]
+
+        return context
 
