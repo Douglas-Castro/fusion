cdf85e34d6d6877425e07912b43a0f4b8c97248b
Douglas-Castro <farias.douglascastro@gmail.com>
2021-03-13 13:08:59 -0300

    :bulb: Add Contact Form and email messages

--- core/views.py
+++ core/views.py
@@ -1,9 +1,14 @@
-from django.views.generic import TemplateView
+from django.views.generic import FormView
+from django.urls import reverse_lazy
+from django.contrib import messages
 from .models import Service, Employee, Feature, Plan
+from .forms import ContactForm
 
 
-class IndexView(TemplateView):
+class IndexView(FormView):
     template_name = 'index.html'
+    form_class = ContactForm
+    success_url = reverse_lazy('index')
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
@@ -14,4 +19,16 @@ class IndexView(TemplateView):
         context['plans'] = Plan.objects.all()
 
         return context
+
+    def form_valid(self, form, *args, **kwargs):
+        form.send_mail()
+        messages.success(self.request, 'E-mail enviado com sucesso!')
+
+        return super(IndexView, self).form_valid(form, *args, **kwargs)
+
+    def form_invalid(self, form, *args, **kwargs):
+        messages.error(self.request, 'Erro ao enviar o e-mail.')
+
+        return super(IndexView, self).form_invalid(form, *args, **kwargs)
+
 
