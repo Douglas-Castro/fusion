3ce50ebc9336d1676a4e9a3241e18b5d91a0fc72
Douglas-Castro <farias.douglascastro@gmail.com>
2021-04-03 00:00:19 -0300

    :bulb: Add gettext in views

--- core/views.py
+++ core/views.py
@@ -1,6 +1,9 @@
 from django.views.generic import FormView
 from django.urls import reverse_lazy
 from django.contrib import messages
+from django.utils.translation import gettext as _
+from django.utils import translation
+
 from .models import Service, Employee, Feature, Plan, Client
 from .forms import ContactForm
 
@@ -12,23 +15,26 @@ class IndexView(FormView):
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
+        lang = translation.get_language()
         context['services'] = Service.objects.order_by('?').all()
         context['employees'] = Employee.objects.order_by('?').all()
         context['featurescolumn1'] = Feature.objects.order_by('?').all()[:3]
         context['featurescolumn2'] = Feature.objects.order_by('?').all()[3:6]
         context['plans'] = Plan.objects.all()
         context['clients'] = Client.objects.order_by('?').all()
+        context['lang'] = lang
+        translation.activate(lang)
 
         return context
 
     def form_valid(self, form, *args, **kwargs):
         form.send_mail()
-        messages.success(self.request, 'E-mail enviado com sucesso!')
+        messages.success(self.request, _('E-mail enviado com sucesso!'))
 
         return super(IndexView, self).form_valid(form, *args, **kwargs)
 
     def form_invalid(self, form, *args, **kwargs):
-        messages.error(self.request, 'Erro ao enviar o e-mail.')
+        messages.error(self.request, _('Erro ao enviar o e-mail.'))
 
         return super(IndexView, self).form_invalid(form, *args, **kwargs)
 
